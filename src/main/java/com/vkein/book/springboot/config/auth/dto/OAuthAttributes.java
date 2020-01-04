4a0802685cc6228dbe07382e97012968afd75c01
jaehee <jaehee23@naver.com>
2020-01-04 16:57:52 +0900

    5장 완료

---
+++ src/main/java/com/vkein/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -0,0 +1,48 @@
+package com.vkein.book.springboot.config.auth.dto;
+
+import com.vkein.book.springboot.domain.user.Role;
+import com.vkein.book.springboot.domain.user.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class OAuthAttributes {
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String name;
+    private String email;
+    private String picture;
+
+    @Builder
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture){
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+
+    public static OAuthAttributes of(String registrationId, String userNameAttributeKey, Map<String, Object> attributes){
+        return ofGoogle(userNameAttributeKey, attributes);
+    }
+
+    private static OAuthAttributes ofGoogle(String userNameAttributeKey, Map<String, Object> attributes){
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeKey).build();
+    }
+
+    public User toEntity() {
+        return User.builder()
+                .name(name)
+                .email(email)
+                .picture(picture)
+                .role(Role.GUEST)
+                .build();
+    }
+}
